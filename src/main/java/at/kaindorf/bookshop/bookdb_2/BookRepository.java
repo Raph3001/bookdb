f2bef75db39b0dfa2c1c1328a72c9a70ebff011b
Rapha <rapha.bader@gmail.com>
2024-09-30 09:44:29 +0200

    removed unneeded classes

--- src/main/java/at/kaindorf/bookshop/bookdb_2/BookRepository.java
+++ src/main/java/at/kaindorf/bookshop/bookdb_2/BookRepository.java
@@ -4,5 +4,4 @@ import at.kaindorf.bookshop.bookdb_2.pojos.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, String> {
-    // You can define custom query methods here if needed
 }
